2cfde2b692fa9e63e5cd60c34adf3d45fa8edb72
Derek Zhu <1and1get2@gmail.com>
2018-01-14 00:02:55 +1300

    update iasl6.1, add default patch source,

--- MaciASL/AppDelegate.m
+++ MaciASL/AppDelegate.m
@@ -32,7 +32,11 @@
 -(void)awakeFromNib{
     log = [NSMutableArray array];
     [NSUserDefaults.standardUserDefaults addObserver:self forKeyPath:@"acpi" options:0 context:NULL];
-    [NSUserDefaults.standardUserDefaults registerDefaults:@{@"theme":@"Light", @"dsdt":@(YES), @"suggest":@(NO), @"acpi":@61, @"context":@(NO), @"isolation":@(NO), @"colorize":@(YES), @"remarks":@(NO), @"optimizations": @(NO), @"werror": @(NO), @"preference": @0, @"font": @{@"name":@"Menlo", @"size": @11}, @"sources":@[@{@"name":@"Sourceforge", @"url":@"http://maciasl.sourceforge.net"}]}];
+    [NSUserDefaults.standardUserDefaults registerDefaults:@{@"theme":@"Light", @"dsdt":@(YES), @"suggest":@(NO), @"acpi":@61, @"context":@(NO), @"isolation":@(NO), @"colorize":@(YES), @"remarks":@(NO), @"optimizations": @(NO), @"werror": @(NO), @"preference": @0, @"font": @{@"name":@"Menlo", @"size": @11}, @"sources":@[
+  @{@"name":@"Sourceforge", @"url":@"http://maciasl.sourceforge.net"},
+  @{@"name":@"OS-X-ACPI-Debug", @"url":@"http://raw.github.com/RehabMan/OS-X-ACPI-Debug/master"}
+  @{@"name":@"RehabMan Laptop", @"url":@"http://raw.github.com/RehabMan/Laptop-DSDT-Patch/master"}
+  @{@"name":@"VoodooI2C-Patches", @"url":@"http://raw.github.com/alexandred/VoodooI2C-Patches/master"}]}];
     NSFontManager.sharedFontManager.target = self;
     NSDictionary *font = [NSUserDefaults.standardUserDefaults objectForKey:@"font"];
     [NSFontManager.sharedFontManager setSelectedFont:[NSFont fontWithName:[font objectForKey:@"name"] size:[[font objectForKey:@"size"] floatValue]] isMultiple:false];
