20d5afb3873c9a0d1a56929b608fd06ef0cef10a
RehabMan <racerrehabman@gmail.com>
2018-05-07 07:16:01 -0700

    add OEM table id handling

--- patchmatic/main.m
+++ patchmatic/main.m
@@ -23,6 +23,8 @@
 
 const char name[] = "patchmatic";
 
+#define countof(x) (sizeof(x)/sizeof(x[0]))
+
 void NSPrintF(NSString *format, ...) {
     va_list args;
     va_start(args, format);
@@ -58,7 +60,7 @@ static void PatchMatic(NSString* strInputFile, NSString* strPatchesFile, NSStrin
     NSPrintF(@"patched result written to '%@'\n", strOutputFile);
 }
 
-void ExtractTables(bool all)
+void ExtractTables(bool all, bool noOemTableId, bool noHotpatchNaming)
 {
     io_service_t expert;
     if ((expert = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("AppleACPIPlatformExpert")))) {
@@ -68,9 +70,41 @@ void ExtractTables(bool all)
                 ////NSPrintF(@"%@\n", table);
                 NSData* aml = [tableset objectForKey:table];
                 if (aml) {
+                    // default rules for naming... (depending on noOemTableId/noHotpatchNaming)
+                    // SSDT -> SSDT-0.aml (or SSDT-0-tblid)
+                    // SSDT-nn where OemTableID does not start with underscore -> SSDT-nn.aml (or SSDT-nn-tblid)
+                    // SSDT-nn where OemTableID starts with underscore -> SSDT-tblid_without_leading_underscore
+                    // others -> tablename.aml
                     NSMutableString* name = [NSMutableString stringWithString:table];
+                    // special case for SSDT.aml to match Clover ACPI/origin extract
+                    if ([name isEqualToString:@"SSDT"]) {
+                        [name setString:@"SSDT-0"];
+                    }
+                    // append table name
+                    if ([name hasPrefix:@"SSDT"]) {
+                        NSString* suffix = [[[NSString alloc] initWithData:[[tableset objectForKey:table] subdataWithRange:NSMakeRange(16, 8)] encoding:NSASCIIStringEncoding] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+                        // table name can have NULs in it, so clean it up
+                        char buffer[9];
+                        [suffix getCString:buffer maxLength:countof(buffer) encoding:NSASCIIStringEncoding];
+                        NSString* clean = [NSString stringWithFormat:@"%s", buffer];
+                        if ([clean length] != 0) {
+                            if ([clean hasPrefix:@"_"] && !noHotpatchNaming) {
+                                // when OemTableID starts with underscore,...
+                                if ([clean length] > 1) {
+                                    [name setString:@"SSDT-"];
+                                    [name appendString:[clean substringFromIndex:1]];
+                                }
+                            } else {
+                                if (!noOemTableId) {
+                                    [name appendString:@"-"];
+                                    [name appendString:clean];
+                                }
+                            }
+                        }
+                    }
                     [name appendString:@".aml"];
-                    [aml writeToFile:name atomically:false];
+                    //NSPrintF(@"name: \"%@\"\n", name);
+                    [aml writeToFile:name atomically:NO];
                 }
             }
         }
@@ -81,40 +115,67 @@ void ExtractTables(bool all)
 #ifndef REGEXTEST
 int main(int argc, const char* argv[]) {
     @autoreleasepool {
-        if (2 == argc && 0 == strcmp(argv[1], "-extract"))
-        {
-            ExtractTables(false);
+        bool extract = false;
+        bool all = false;
+        bool noOemTableId = false;
+        bool noHotpatchNaming = false;
+        int i = 1;
+        for (; i < argc; i++) {
+            const char* arg = argv[i];
+            if (0 == strcmp(arg, "-extract")) {
+                extract = true;
+            }
+            else if (0 == strcmp(arg, "-extractall")) {
+                extract = true;
+                all = true;
+            }
+            else if (0 == strcmp(arg, "-nooemtableid")) {
+                noOemTableId = true;
+            }
+            else if (0 == strcmp(arg, "-nohotpatchnaming")) {
+                noHotpatchNaming = true;
+            }
+            else if (0 == strcmp(arg, "-raw")) {
+                noOemTableId = true;
+                noHotpatchNaming = true;
+            }
+            else {
+                break;
+            }
+        }
+        if (extract) {
+            ExtractTables(all, noOemTableId, noHotpatchNaming);
             return 0;
         }
-        if (2 == argc && 0 == strcmp(argv[1], "-extractall"))
-        {
-            ExtractTables(true);
-            return 0;
-        }
-        if (argc < 3)
+        if (argc - i < 2 || argc - i > 3)
         {
             NSString* usage =
                @"Usage: patchmatic <dsl-input> <patches-file> [<dsl-output>]\n"
                 " where:\n"
                 "   <dsl-input>     name of ASCII DSL input file (output from iasl -d)\n"
                 "   <patches-file>  name of file containing patches to apply to <dsl-input>\n"
-                "   <dsl-output>    name of patched output file (to be compiled with iasl)\n"
-                ">> Patches <dsl-input> with <patches-file> and produces patched <dsl-output>\n"
+                "   [dsl-output]    optional name of patched output file (to be compiled with iasl)\n"
+                ">> Patches <dsl-input> with <patches-file> and produces patched [dsl-output]\n"
+                "   (when [dsl-output] not specified, output is to <dsl-input>)\n"
                 "\n"
                 "-OR-\n"
                 "\n"
-                "Usage: patchmatic -extract\n"
-                "Usage: patchmatic -extractall\n"
+                "Usage: patchmatic [options] -extract\n"
+                "Usage: patchmatic [options] -extractall\n"
                 ">> Extracts loaded ACPI binaries from ioreg\n"
                 "  -extract will extract just DSDT/SSDTs\n"
                 "  -extractall will extract all ACPI tables\n"
+                ">> Options (may appear after -extract/-extractall or before)\n"
+                "  -nooemtableid: omits OEM table ID from file names\n"
+                "  -nohotpatchnaming: no OEM table ID handling based on RM hotpatch conventions\n"
+                "  -raw: same as both -nooemtableid and -nohotpatchnaming\n"
                 "\n";
             NSPrintF(@"%@", usage);
             return 1;
         }
-        NSString* strInputFile = [NSString stringWithCString:argv[1] encoding:NSASCIIStringEncoding];
-        NSString* strPatchesFile = [NSString stringWithCString:argv[2] encoding:NSASCIIStringEncoding];
-        NSString* strOutputFile = argc > 3 ? [NSString stringWithCString:argv[3] encoding:NSASCIIStringEncoding] : strInputFile;
+        NSString* strInputFile = [NSString stringWithCString:argv[i+0] encoding:NSASCIIStringEncoding];
+        NSString* strPatchesFile = [NSString stringWithCString:argv[i+1] encoding:NSASCIIStringEncoding];
+        NSString* strOutputFile = argc > 3 ? [NSString stringWithCString:argv[i+2] encoding:NSASCIIStringEncoding] : strInputFile;
         PatchMatic(strInputFile, strPatchesFile, strOutputFile);
     }
     return 0;
